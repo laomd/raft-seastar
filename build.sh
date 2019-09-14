6d7c0d0100b3b95fdc1798db59a3bb2754c94e64
laomd <965524991@qq.com>
2019-09-14 17:05:03 +0000

    use seastar

--- build.sh
+++ build.sh
@@ -4,20 +4,30 @@ set -e
 export CC=clang
 export CXX=clang++
 
-build_type=""
-tmp=$(echo $1 | tr [A-Z] [a-z])
-if [ $# == 0 ] || [ "$tmp" == "release" ]; then
+build_type="release"
+target="all"
+while getopts "b:t:" opts; do
+    case $opts in
+        b) build_type=$OPTARG ;;
+        t) target=$OPTARG ;;
+        ?) ;;
+    esac
+done
+
+build_type=$(echo $build_type | tr [A-Z] [a-z])
+if [ "$build_type" == "release" ]; then
     build_type="Release"
-elif [ "$tmp" == "debug" ]; then
+elif [ "$build_type" == "debug" ]; then
     build_type="Debug"
 else
     echo "invalid build type"
+    exit 1
 fi
-if [ "$build_type" != "" ]; then
-    NUM_CORES=$(cat /proc/cpuinfo | grep "processor" | wc -l)
-    build_dir="build/$build_type"
-    rm -rf build/* && rm -rf build && mkdir -p $build_dir && cd $build_dir
-    cmake ../../ -DCMAKE_BUILD_TYPE=$build_type
-    make -j $NUM_CORES
-    cd - && mv build /var && ln -s /var/build .
-fi
+NUM_CORES=$(cat /proc/cpuinfo | grep "processor" | wc -l)
+echo "-- build with $NUM_CORES cores, type: $build_type, target: $target"
+
+build_dir="build/$build_type"
+rm -rf build/* && rm -rf build && mkdir -p $build_dir && cd $build_dir
+cmake ../../ -DCMAKE_BUILD_TYPE=$build_type
+make -j $NUM_CORES $target
+cd - && mv build /var && ln -s /var/build .
