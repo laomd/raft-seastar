f6f7e0b9870998aba826fa017d231cd18840908c
laomd <965524991@qq.com>
2020-01-30 03:50:11 +0000

    use popen to create subprocess

--- tests/test_runner.hh
+++ tests/test_runner.hh
@@ -25,54 +25,59 @@ public:
 
   ~TestRunner() { clean_up(); }
 
-  void clean_up() { system("kill -9 `pidof raft_server`"); }
+  void clean_up() {
+    for (auto fp : server_subpros_) {
+      fclose(fp);
+    }
+  }
 
-  seastar::future<> checkOneLeader() {
-    return seastar::repeat([this] {
-      return seastar::sleep(electionTimeout_).then([this] {
-        std::cout << "checking leaders..." << std::endl;
-        auto leaders = seastar::make_shared<std::map<term_t, id_t>>();
-        return seastar::do_for_each(
-                   addrs_,
-                   [this, leaders](const auto &addr) {
-                     seastar::rpc::client_options opts;
-                     opts.send_timeout_data = false;
-                     auto stub = seastar::make_shared<RaftClient>(proto_, opts, addr);
-                     auto func = proto_.make_client<
-                         seastar::future<term_t, id_t, bool>()>(3);
-                     return func(*stub)
-                         .then([leaders](term_t term, id_t id, bool isLeader) {
-                           if (isLeader) {
-                            //  BOOST_REQUIRE(leaders->find(term) ==
-                            //                    leaders->end() ||
-                            //                (*leaders)[term] == id);
-                             (*leaders)[term] = id;
-                           }
-                         })
-                         .finally([stub] {
-                           return stub->stop().finally(
-                               [stub] { return seastar::make_ready_future(); });
-                         })
-                         .handle_exception_type(
-                             ignore_exception<seastar::rpc::closed_error>)
-                         .handle_exception_type(
-                             ignore_exception<std::system_error>);
-                   })
-            .then_wrapped([this, leaders](auto &&fut) {
-              std::cout << "leaders: ";
-              if (leaders->empty()) {
-                std::cout << "none" << std::endl;
-                return seastar::stop_iteration::no;
-              } else {
-                for (auto &&item : *leaders) {
-                  std::cout << item.first << "->" << item.second << ' ';
-                }
-                std::cout << std::endl;
-                return seastar::stop_iteration::yes;
-              }
-            });
-      });
-    });
+  seastar::future<> checkOneLeader(uint32_t times = 100) {
+    auto leaders = seastar::make_shared<std::map<term_t, id_t>>();
+    return seastar::do_until(
+        [times]() mutable { return !(times--); },
+        [this, leaders] {
+          return seastar::sleep(electionTimeout_).then([this, leaders] {
+            std::cout << "checking leaders..." << std::endl;
+            return seastar::do_for_each(
+                       addrs_,
+                       [this, leaders](const auto &addr) {
+                         seastar::rpc::client_options opts;
+                         opts.send_timeout_data = false;
+                         auto stub = seastar::make_shared<RaftClient>(
+                             proto_, opts, addr);
+                         auto func = proto_.make_client<
+                             seastar::future<term_t, id_t, bool>()>(3);
+                         return func(*stub)
+                             .then([leaders](term_t term, id_t id,
+                                             bool isLeader) {
+                               if (isLeader) {
+                                 BOOST_REQUIRE(leaders->find(term) ==
+                                                   leaders->end() ||
+                                               (*leaders)[term] == id);
+                                 (*leaders)[term] = id;
+                               }
+                             })
+                             .finally([stub] {
+                               return stub->stop().finally([stub] {
+                                 return seastar::make_ready_future();
+                               });
+                             })
+                             .handle_exception_type(
+                                 ignore_exception<seastar::rpc::closed_error>)
+                             .handle_exception_type(
+                                 ignore_exception<std::system_error>);
+                       })
+                .then_wrapped([this, leaders](auto &&fut) {
+                  std::cout << "leader: ";
+                  if (leaders->empty()) {
+                    std::cout << "none" << std::endl;
+                  } else {
+                    auto it = leaders->rbegin();
+                    std::cout << it->first << "->" << it->second << std::endl;
+                  }
+                });
+          });
+        });
   }
 
 private:
@@ -83,7 +88,6 @@ private:
   }
 
   void start_servers() {
-    clean_up();
     std::stringstream peers;
     size_t num_servers = addrs_.size();
     for (const auto &addr : addrs_) {
@@ -99,11 +103,15 @@ private:
       std::string tmp = std::to_string(i);
       std::string cmd = bin + " -i " + tmp + " > " + tmp + ".log &";
       std::cout << "run cmd: " << cmd << std::endl;
-      system(cmd.c_str());
+      FILE *fp = nullptr;
+      while (!(fp = popen(cmd.c_str(), "r")))
+        ;
+      server_subpros_.emplace_back(fp);
     }
   }
 
   std::deque<seastar::ipv4_addr> addrs_;
+  std::deque<FILE *> server_subpros_;
   rpc_protocol proto_;
   ms_t electionTimeout_, heartbeat_;
 };
